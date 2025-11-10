de092478d4f8cdc943e6059602a65387e38d089b
yohanoh <12dhdygks@naver.com>
2025-11-10 01:12:10 +0000

    finish part4

--- polls/views.py
+++ polls/views.py
@@ -1,25 +1,51 @@
-from django.http import HttpResponse
+from django.db.models import F
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
-
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {"latest_question_list": latest_question_list}
-
-    return render(request, "polls/index.html", context)
+from .models import Question, Choice
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    # template인 index.html 로 넘겨줄 argument 명을 직접 정의
+    context_object_name = "lastest_question_list"
 
-    return render(request, "polls/detail.html", {"question": question})
+    def get_queryset(self):
+        # lastest_question_list 로 전달할 value
+        return Question.objects.order_by("-pub_date")[:5]
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class DetailView(generic.DetailView):
+    model = Question
+    # template 명은 <app_name>/<model_name>_detail.html 로 자동으로 찾음
+    # 이를 회피하기 위해 명시적으로 지정
+    template_name = "polls/detail.html"
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoestNotExist):
+        return render(
+            "polls/detail.html", {
+                "question": question,
+                "error_message": "You didn't select a choice",
+            },
+        )
+    else:
+        # Increase choice's db data votes by 1
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+
+        # redirect url -> /polls/<question.id>/results/
+        return HttpResponseRedirect(
+            reverse("polls:results", args=(question.id,))
+        )
