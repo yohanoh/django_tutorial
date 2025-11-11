fa95b80e08a904f7f9a3673338ad1869ae58044a
yohanoh <12dhdygks@naver.com>
2025-11-11 06:30:11 +0000

    finish part8

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('polls/', include("polls.urls")),
     path('admin/', admin.site.urls),
-]
+] + debug_toolbar_urls()
