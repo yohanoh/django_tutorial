856fbdd620977ab9f75847bca26e9a3c3b86dfbd
yohanoh <12dhdygks@naver.com>
2025-11-11 06:30:28 +0000

    Revert "finish part8"
    This reverts commit fa95b80e08a904f7f9a3673338ad1869ae58044a.
    디버깅용으로 사용하는 django-debug-toolbar 테스트용임으로 이전 commit 은
    revert

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-INTERNAL_IPS = ["127.0.0.1"]
 
 # Application definition
 
@@ -39,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'debug_toolbar'
 ]
 
 MIDDLEWARE = [
@@ -50,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware'
 ]
 
 ROOT_URLCONF = 'mysite.urls'
